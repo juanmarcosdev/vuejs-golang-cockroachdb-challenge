1356e0da7730c513593e0dad678ee1cede7360ce
juanmarcos935 <juan.marcos.caicedo@correounivalle.edu.co>
2020-06-20 21:23:03 -0500

    Avanzando en el Backend; ya se obtiene info de servidores. Faltan los demas atributos (persistencia + webscrapping

--- backend/handlers.go
+++ backend/handlers.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+
 	"github.com/valyala/fasthttp"
 )
 
+type JSONDef struct {
+	Servers          []ServersDef `json:"servers"`
+	ServersChanged   bool         `json:"servers_changed"`
+	SslGrade         string       `json:"ssl_grade"`
+	PreviousSslGrade string       `json:"previous_ssl_grade"`
+	Logo             string       `json:"logo"`
+	Title            string       `json:"title"`
+	IsDown           bool         `json:"is_down"`
+}
+
+type ServersDef struct {
+	Address  string `json:"address"`
+	SslGrade string `json:"ssl_grade"`
+	Country  string `json:"country"`
+	Owner    string `json:"owner"`
+}
+
 func GetQueriedDomains(ctx *fasthttp.RequestCtx) {
-	ctx.WriteString("Hello")
+	ctx.WriteString("Get funcionando!")
+	// mc := &MethodCaller{}
+	// endpoints := mc.GetSSLLabsInfo("netflix.com")
 }
 
 func PostDomainAndGetInfo(ctx *fasthttp.RequestCtx) {
@@ -13,7 +35,28 @@ func PostDomainAndGetInfo(ctx *fasthttp.RequestCtx) {
 	if !err {
 		ctx.SetStatusCode(fasthttp.StatusBadRequest)
 	}
-	md := &MethodCaller{}
-	country, organization := md.ObtainServerCountryAndOrganization(domain)
-	ctx.WriteString(country + organization)
+	mc := &MethodCaller{}
+	endpoints := mc.GetSSLLabsInfo(domain)
+	servers_def := make([]ServersDef, len(endpoints))
+	var country string
+	var organization string
+	for i, e := range endpoints {
+		country, organization = mc.ObtainIPCountryAndOrganization(e.IPAddress)
+		servers_def[i] = ServersDef{
+			Address:  e.IPAddress,
+			SslGrade: e.Grade,
+			Country:  country,
+			Owner:    organization,
+		}
+	}
+	jsonp := &JSONDef{
+		Servers: servers_def,
+	}
+	jsons, err2 := json.Marshal(jsonp)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	ctx.WriteString(string(jsons))
+	// country, organization := mc.ObtainIPCountryAndOrganization(domain)
+	// ctx.WriteString(country + "\n" + organization)
 }
--- backend/methods.go
+++ backend/methods.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os/exec"
 	"strings"
+	"time"
 )
+
+type Domain struct {
+	Host      string     `json:"host,omitempty"`
+	Endpoints []Endpoint `json:"endpoints,omitempty"`
+}
+
+type Endpoint struct {
+	IPAddress string `json:"ipAddress,omitempty"`
+	Grade     string `json:"grade,omitempty"`
+}
 
 type MethodCaller struct{}
 
-func (md *MethodCaller) ObtainServerCountryAndOrganization(domain string) (string, string) {
-	commandS1 := []string{"-c", "whois " + domain + " | grep -i 'registrant country' | cut -d ' ' -f 3"}
+func (mc *MethodCaller) ObtainIPCountryAndOrganization(ip string) (string, string) {
+	commandS1 := []string{"-c", "whois " + ip + " | grep -i 'country' | cut -d ':' -f 2"}
 	cmd1 := exec.Command("/bin/bash", commandS1...)
-	commandS2 := []string{"-c", "whois " + domain + " | grep -i 'registrant organization' | awk -F':' '{print $2}'"}
+	commandS2 := []string{"-c", "whois " + ip + " | grep -i 'orgname' | cut -d ':' -f 2"}
 	cmd2 := exec.Command("/bin/bash", commandS2...)
 	output1, err1 := cmd1.CombinedOutput()
 	output2, err2 := cmd2.CombinedOutput()
@@ -23,6 +37,21 @@ func (md *MethodCaller) ObtainServerCountryAndOrganization(domain string) (strin
 	}
 	output1Str := string(output1)
 	output2Str := string(output2)
-	return output1Str, strings.TrimSpace(output2Str)
+	return strings.TrimSpace(output1Str), strings.TrimSpace(output2Str)
 
+}
+
+func (mc *MethodCaller) GetSSLLabsInfo(domain string) []Endpoint {
+	var clienteHTTP = &http.Client{Timeout: 20 * time.Second}
+	resp, err := clienteHTTP.Get("https://api.ssllabs.com/api/v3/analyze?host=" + domain)
+	if err != nil {
+		log.Fatal(err)
+	}
+	decoder := json.NewDecoder(resp.Body)
+	dom := &Domain{}
+	errdef := decoder.Decode(dom)
+	if errdef != nil {
+		log.Fatal(errdef)
+	}
+	return dom.Endpoints
 }
