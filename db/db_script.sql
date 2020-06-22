3949d6829294add95d253d7a14a3976c8bcbbe64
juanmarcos935 <juan.marcos.caicedo@correounivalle.edu.co>
2020-06-22 00:45:32 -0500

    Corrigiendo

--- db/db_script.sql
+++ db/db_script.sql
@@ -1,3 +1,13 @@
+CREATE USER endpoints_admin;
+
+CREATE DATABASE endpoints_db;
+
+SET DATABASE = endpoints_db;
+
+SET TIMEZONE = 'America/Bogota';
+
+GRANT ALL ON DATABASE endpoints_db TO endpoints_admin;
+
 CREATE TABLE endpoint_table(
     dominio varchar NOT NULL,
     info_servers JSONB NOT NULL,
@@ -5,3 +15,4 @@ CREATE TABLE endpoint_table(
     hora_consulta TIMESTAMP NOT NULL,
     CONSTRAINT pk_endpoint PRIMARY KEY (dominio)
 );
+
