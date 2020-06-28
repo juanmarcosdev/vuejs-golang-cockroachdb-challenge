4549ce6e920202150584a73fd4818168b7cdac77
juanmarcos935 <juan.marcos.caicedo@correounivalle.edu.co>
2020-06-27 22:05:38 -0500

    Empezando frontend con Header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <component-header />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ComponentHeader from "@/components/ComponentHeader";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { ComponentHeader }
+};
 </script>
 
 <style>
